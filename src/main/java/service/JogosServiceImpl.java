357219393942c83845e99da2bbceafdae1e5454e
Lucas Leme de Moura <lucaslememoura@gmail.com>
2022-11-21 18:25:01 -0300

    criando service

--- src/main/java/service/JogosServiceImpl.java
+++ src/main/java/service/JogosServiceImpl.java
@@ -3,7 +3,7 @@ package service;
 import dao.AbstractDaoGeneric;
 import model.Jogo;
 
-public class JogosServiceImpl extends CrudServiceImp<Jogo, Integer>{
+public class JogosServiceImpl extends CrudServiceImp<Jogo, Integer> implements JogosService{
 
     public JogosServiceImpl(AbstractDaoGeneric<Jogo, Integer> dao) {
         super(dao);
